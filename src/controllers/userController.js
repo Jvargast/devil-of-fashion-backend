81307f6e138222a21934d814063ad2343c6e1845
José Vargas <josevargas@MacBook-Air-de-Jose-3.local>
2022-07-01 19:33:06 -0400

    user authentication and forgot password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAsyncErrors");
 const User = require("../models/User");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 //Register a user
 exports.registerUser = catchAsyncError(async(req,res,next) => {
@@ -51,3 +53,66 @@ exports.logOut = catchAsyncError(async(req,res,next)=> {
     })
 })
 
+
+//Forgot Password
+exports.forgotPassword = catchAsyncError(async(req,res,next) => {
+    const user = await User.findOne({email:req.body.email});
+
+
+    if(!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    //Get ResetPassword Token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({validateBeforeSave: false});
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+    const message  = `El link para cambiar contraseña es: \n\n ${resetPasswordUrl} \n\nSi no solicitaste un cambio de contraseña, por favor ignora este correo.`; 
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `DevilOfFashion - Recuperación de contraseña`,
+            message
+        });
+
+        res.status(200).json({
+            success:true,
+            message: `Email enviado a ${user.email} exitosamente`
+        })
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({validateBeforeSave: false});
+
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+
+//Reset password
+exports.resetPassword = catchAsyncError(async(req,res,next) =>{
+
+    //creating token hash
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
+
+    const user = await User.findOne({resetPasswordToken,resetPasswordExpire:{$gt:Date.now()}});
+
+    if(!user) {
+        return next(new ErrorHandler("El link es inválido o ha expirado", 404));
+    };
+
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Las contraseñas no coinciden", 404));
+    };
+
+    user.password = req.body.password;
+    user.resetPasswordExpire = undefined;
+    user.resetPasswordToken = undefined;
+
+    await user.save()
+    sendToken(user,200,res);
+})
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { registerUser, loginUser, logOut } = require("../controllers/userController");
+const { registerUser, loginUser, logOut, forgotPassword, resetPassword } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/registro").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logOut);
+
+
 
 module.exports = router;
